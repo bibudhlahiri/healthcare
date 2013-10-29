9c2934632bb10341f4c20128bcec295ef53aace9
Bibudh Lahiri <bibudhlahiri@gmail.com>
2013-10-28 22:25:11 -0400

    Added glmnet in create_sparse_feature_matrix

--- code/basic_stats.R
+++ code/basic_stats.R
@@ -478,12 +478,19 @@ claim_amount_change_type <- function()
  {
    con <- dbConnect(PostgreSQL(), user="postgres", password = "impetus123",  
                    host = "localhost", port="5432", dbname = "DE-SynPUF")
-   statement <- paste("select b1.DESYNPUF_ID, tcdc.dgns_cd
+   statement <- paste("select b1.DESYNPUF_ID, tcdc.dgns_cd, 
+                       case when b2.MEDREIMB_IP > b1.MEDREIMB_IP then 1 else 0
+                       end as change_type
                        from beneficiary_summary_2008 b1, beneficiary_summary_2009 b2, transformed_claim_diagnosis_codes tcdc
                        where b1.DESYNPUF_ID = b2.DESYNPUF_ID
                        and b1.DESYNPUF_ID = tcdc.DESYNPUF_ID
                        and to_char(tcdc.clm_thru_dt, 'YYYY') = '2008'
-                       order by b1.DESYNPUF_ID", sep = "")
+                       and tcdc.dgns_cd in (select tcdc1.dgns_cd
+                                            from transformed_claim_diagnosis_codes tcdc1
+                                            where to_char(tcdc1.clm_thru_dt, 'YYYY') = '2008'
+                                            group by tcdc1.dgns_cd
+                                            order by count(distinct tcdc1.DESYNPUF_ID) desc
+                                            limit 20)", sep = "")
   res <- dbSendQuery(con, statement);
   df <- fetch(res, n = -1) 
   all_dgns_codes <- unique(df$dgns_cd)
@@ -502,6 +509,7 @@ claim_amount_change_type <- function()
        patient_id <- df[i, "desynpuf_id"]
     }
     sparse_df[row_index, "patient_id"] <- df[i, "desynpuf_id"]
+    sparse_df[row_index, "change_type"] <- df[i, "change_type"]
     sparse_df[row_index, df[i, "dgns_cd"]] <- 1   
     if (i %% 2000 == 0)
     {
@@ -534,16 +542,20 @@ create_sparse_feature_matrix <- function()
  {
    library(Matrix)
    library(hash)
+   library(glmnet)
    con <- dbConnect(PostgreSQL(), user="postgres", password = "impetus123",  
                    host = "localhost", port="5432", dbname = "DE-SynPUF")
-   statement <- paste("select b1.desynpuf_id, tcdc.dgns_cd
+   statement <- paste("select distinct a.DESYNPUF_ID, a.dgns_cd, a.change_type
+                       from (select b1.DESYNPUF_ID, tcdc.dgns_cd, 
+                             case when b2.MEDREIMB_IP > b1.MEDREIMB_IP then 1 else 0
+                             end as change_type
                        from beneficiary_summary_2008 b1, beneficiary_summary_2009 b2, transformed_claim_diagnosis_codes tcdc
                        where b1.DESYNPUF_ID = b2.DESYNPUF_ID
                        and b1.DESYNPUF_ID = tcdc.DESYNPUF_ID
-                       and to_char(tcdc.clm_thru_dt, 'YYYY') = '2008'
-                       order by b1.DESYNPUF_ID", sep = "")
+                       and to_char(tcdc.clm_thru_dt, 'YYYY') = '2008') a", sep = "")
   res <- dbSendQuery(con, statement);
   df <- fetch(res, n = -1)
+  cat(paste("nrow(df) = ", nrow(df), ", ncol(df) = ", ncol(df), "\n", sep = ""))
   all_beneficiaries <- unique(df$desynpuf_id)
   all_dgns_codes <- unique(df$dgns_cd)
   n_dgns_codes <- length(all_dgns_codes)
@@ -555,8 +567,11 @@ create_sparse_feature_matrix <- function()
   df$bene_num <- apply(df, 1, function(row)lookup_bene_num(row["desynpuf_id"]))
   df$dgns_num <- apply(df, 1, function(row)lookup_dgns_num(row["dgns_cd"]))
   sparse_mat <- sparseMatrix(i = df$bene_num, j = df$dgns_num, x = 1, dimnames=list(1:n_beneficiaries,1:n_dgns_codes))
-  
+  #print(sparse_mat[1:10, 1:10])
+  cat(paste("nrow(sparse_mat) = ", nrow(sparse_mat), ", ncol(sparse_mat) = ", ncol(sparse_mat), "\n", sep = ""))
   dbDisconnect(con)
+  fit = glmnet(sparse_mat, df$change_type, family="binomial")
+  return(fit)
 }
 
 
