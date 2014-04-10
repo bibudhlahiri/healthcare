268d00aa2909e23661e07d57fc3070427f4f89b3
Bibudh Lahiri <bibudhlahiri@gmail.com>
2014-04-10 11:40:07 -0400

    Changed prepare_conditionals_for_conditions_proc_zero to perform queries in-memory with SQLite and sqldf

--- code/fast_bayesian_fraud_detection.R
+++ code/fast_bayesian_fraud_detection.R
@@ -86,7 +86,7 @@ prepare_conditionals_for_conditions_proc_zero <- function()
   procedure_priors <- read.csv("/Users/blahiri/healthcare/documents/fraud_detection/bayesian/procedure_priors.csv")
   n_procs <- nrow(procedure_priors)
   cpt_conditions_proc_zero <- list()
-  #n_procs <- 10
+  n_procs <- 20
   cat(paste("n_procs = ", n_procs, "\n", sep = ""))
 
   statement <- "select count(distinct b.desynpuf_id)
@@ -95,28 +95,41 @@ prepare_conditionals_for_conditions_proc_zero <- function()
                 and to_char(tcpc.clm_thru_dt, 'YYYY') = '2008'"
   n_benefs <- as.numeric(dbGetQuery(con, statement))
 
+  statement <- "select * from transformed_claim_diagnosis_codes tcdc where tcdc.clm_thru_year = '2008'"
+  res <- dbSendQuery(con, statement)
+  transformed_claim_diagnosis_codes <- fetch(res, n = -1)
+
+  statement <- "select * from transformed_claim_prcdr_codes tcpc where to_char(tcpc.clm_thru_dt, 'YYYY') = '2008'"
+  res <- dbSendQuery(con, statement)
+  transformed_claim_prcdr_codes <- fetch(res, n = -1)
+
+  statement <- "select * from beneficiary_summary_2008" 
+  res <- dbSendQuery(con, statement)
+  beneficiary_summary_2008 <- fetch(res, n = -1)
+
   for (i in 1:n_procs)
   {
     this_proc <- procedure_priors[i, "prcdr_cd"]
     proc_count <- procedure_priors[i, "count"]
 
     #Diagnosed conditions for people who did NOT do this procedure but did some other procedure
-    statement <- paste("select tcdc.dgns_cd, count(distinct b.desynpuf_id)
+    #statement <- paste("select tcdc.dgns_cd, count(distinct b.desynpuf_id)
+    cpt_this_dc <- sqldf(paste("select tcdc.dgns_cd, count(distinct b.desynpuf_id)
                         from transformed_claim_diagnosis_codes tcdc, beneficiary_summary_2008 b
                         where tcdc.desynpuf_id = b.desynpuf_id
                         and tcdc.clm_thru_year = '2008'
-                        and not exists (select 1 from transformed_claim_prcdr_codes tcpc
-                                        where to_char(tcpc.clm_thru_dt, 'YYYY') = '2008'
-                                        and tcpc.desynpuf_id = b.desynpuf_id
+                        and not exists (select 1 from transformed_claim_prcdr_codes tcpc ", 
+                                        #where to_char(tcpc.clm_thru_dt, 'YYYY') = '2008'
+                                        " where tcpc.desynpuf_id = b.desynpuf_id
                                         and tcpc.prcdr_cd = '", this_proc, "') 
-                        and exists (select 1 from transformed_claim_prcdr_codes tcpc1
-                        where to_char(tcpc1.clm_thru_dt, 'YYYY') = '2008'
-                        and tcpc1.desynpuf_id = b.desynpuf_id
-                        and tcpc1.prcdr_cd <> '", this_proc, "')
-                        group by tcdc.dgns_cd
-                        order by count(distinct b.desynpuf_id) desc", sep = "")
-    res <- dbSendQuery(con, statement)
-    cpt_this_dc <- fetch(res, n = -1)
+                        and exists (select 1 from transformed_claim_prcdr_codes tcpc1 ", 
+                                    #where to_char(tcpc1.clm_thru_dt, 'YYYY') = '2008'
+                                   " where tcpc1.desynpuf_id = b.desynpuf_id
+                                   and tcpc1.prcdr_cd <> '", this_proc, "')
+                                   group by tcdc.dgns_cd
+                                   order by count(distinct b.desynpuf_id) desc", sep = ""), drv = "SQLite", dbname = ":memory:")
+    #res <- dbSendQuery(con, statement)
+    #cpt_this_dc <- fetch(res, n = -1)
     diag_cpts <- cpt_this_dc$count/(n_benefs - proc_count)
     names(diag_cpts) <- paste("diag_", cpt_this_dc$dgns_cd, sep = "")
     
@@ -125,25 +138,28 @@ prepare_conditionals_for_conditions_proc_zero <- function()
     names_for_chronic_cpts <- c()
     for (chronic_condition in chronic_conditions)
     {
-      statement <- paste("select count(*)
+      #statement <- paste("select count(*)
+      proc_and_cc <- sqldf(paste("select count(*)
                           from beneficiary_summary_2008 b
                           where ", chronic_condition, " = '1'
-                          and not exists (select 1 from transformed_claim_prcdr_codes tcpc
-                                          where to_char(tcpc.clm_thru_dt, 'YYYY') = '2008'
-                                          and tcpc.desynpuf_id = b.desynpuf_id
-                                          and tcpc.prcdr_cd = '", this_proc, "') 
-                          and exists (select 1 from transformed_claim_prcdr_codes tcpc1
-                                      where to_char(tcpc1.clm_thru_dt, 'YYYY') = '2008'
-                                      and tcpc1.desynpuf_id = b.desynpuf_id
+                          and not exists (select 1 from transformed_claim_prcdr_codes tcpc ", 
+                                          #where to_char(tcpc.clm_thru_dt, 'YYYY') = '2008'
+                                          " where tcpc.desynpuf_id = b.desynpuf_id
+                                          and tcpc.prcdr_cd = '", this_proc, "')  
+                          and exists (select 1 from transformed_claim_prcdr_codes tcpc1 ", 
+                                      #where to_char(tcpc1.clm_thru_dt, 'YYYY') = '2008'
+                                      " where tcpc1.desynpuf_id = b.desynpuf_id
                                       and tcpc1.prcdr_cd <> '", this_proc, "')", 
-                         sep = "")
-      proc_and_cc <- as.numeric(dbGetQuery(con, statement))
+                         sep = ""), drv = "SQLite", dbname = ":memory:")
+      #proc_and_cc <- as.numeric(dbGetQuery(con, statement))
+      proc_and_cc <- as.numeric(proc_and_cc)
       if (proc_and_cc > 0)
       {
         chronic_cpts <- append(chronic_cpts, proc_and_cc)
         names_for_chronic_cpts <- append(names_for_chronic_cpts, chronic_condition)
       }
     }
+     
     chronic_cpts <- chronic_cpts/(n_benefs - proc_count)
 
     this_proc <- paste("proc_", this_proc, sep = "")
