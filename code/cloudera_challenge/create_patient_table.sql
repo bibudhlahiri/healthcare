adf72fbac4e75101c4ff8b41cd42cf77eda31ae1
Bibudh Lahiri <bibudhlahiri@gmail.com>
2014-06-20 15:03:22 -0400

    Create table scripts first commit

---
+++ code/cloudera_challenge/create_patient_table.sql
@@ -0,0 +1,17 @@
+﻿drop table if exists patients;
+
+create table patients
+(
+  patient_id bigint,
+  age_group varchar(100),
+  gender  varchar(2),
+  income_range varchar(100)
+);
+
+copy patients
+from '/Users/blahiri/healthcare/data/cloudera_challenge/patients.csv' 
+WITH CSV HEADER DELIMITER ',';
+
+--CREATE INDEX idx_patients_patient_id ON patients (patient_id);
+
+--select count(*) from patients;
---
+++ code/cloudera_challenge/create_procedure_table.sql
@@ -0,0 +1,62 @@
+﻿drop table if exists patient_procedure;
+
+create table patient_procedure
+(
+  proc_date varchar(100),
+  patient_id bigint,
+  procedure_id  varchar(100),
+  column4 varchar(10),
+  column5 varchar(10)
+);
+
+copy patient_procedure
+from '/Users/blahiri/healthcare/data/cloudera_challenge/PCDR2011/PCDR1101.csv' 
+WITH CSV HEADER DELIMITER ',';
+
+copy patient_procedure
+from '/Users/blahiri/healthcare/data/cloudera_challenge/PCDR2011/PCDR1102.csv' 
+WITH CSV HEADER DELIMITER ',';
+
+copy patient_procedure
+from '/Users/blahiri/healthcare/data/cloudera_challenge/PCDR2011/PCDR1103.csv' 
+WITH CSV HEADER DELIMITER ',';
+
+copy patient_procedure
+from '/Users/blahiri/healthcare/data/cloudera_challenge/PCDR2011/PCDR1104.csv' 
+WITH CSV HEADER DELIMITER ',';
+
+copy patient_procedure
+from '/Users/blahiri/healthcare/data/cloudera_challenge/PCDR2011/PCDR1105.csv' 
+WITH CSV HEADER DELIMITER ',';
+
+copy patient_procedure
+from '/Users/blahiri/healthcare/data/cloudera_challenge/PCDR2011/PCDR1106.csv' 
+WITH CSV HEADER DELIMITER ',';
+
+copy patient_procedure
+from '/Users/blahiri/healthcare/data/cloudera_challenge/PCDR2011/PCDR1107.csv' 
+WITH CSV HEADER DELIMITER ',';
+
+copy patient_procedure
+from '/Users/blahiri/healthcare/data/cloudera_challenge/PCDR2011/PCDR1108.csv' 
+WITH CSV HEADER DELIMITER ',';
+
+copy patient_procedure
+from '/Users/blahiri/healthcare/data/cloudera_challenge/PCDR2011/PCDR1109.csv' 
+WITH CSV HEADER DELIMITER ',';
+
+copy patient_procedure
+from '/Users/blahiri/healthcare/data/cloudera_challenge/PCDR2011/PCDR1110.csv' 
+WITH CSV HEADER DELIMITER ',';
+
+copy patient_procedure
+from '/Users/blahiri/healthcare/data/cloudera_challenge/PCDR2011/PCDR1111.csv' 
+WITH CSV HEADER DELIMITER ',';
+
+copy patient_procedure
+from '/Users/blahiri/healthcare/data/cloudera_challenge/PCDR2011/PCDR1112.csv' 
+WITH CSV HEADER DELIMITER ',';
+
+CREATE INDEX idx_patient_procedure_patient_id ON patient_procedure (patient_id);
+
+select count(1) from patient_procedure;
