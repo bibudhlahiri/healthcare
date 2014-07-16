6619fb217d9b3c1d8183de9f23a80dacc5f3e6c6
Bibudh Lahiri <bibudhlahiri@gmail.com>
2014-07-16 12:17:19 -0400

    First commit of convert_to_wide_format.sql

---
+++ code/recomm_system/convert_to_wide_format.sql
@@ -0,0 +1,20 @@
+﻿drop function if exists health_recomm.convert_to_wide_format();
+CREATE FUNCTION health_recomm.convert_to_wide_format() RETURNS void AS $$ 
+
+ declare
+   create_table_statement varchar(200000) := 'create table dense_matrix (desynpuf_id varchar(100), ';
+   curFeatures cursor for 
+     select *
+     from health_recomm.features;
+ begin
+   for rec_feature in curFeatures loop
+      create_table_statement := create_table_statement || rec_feature.feature_name || ' varchar(2),';
+   end loop;
+   --Trim last comma
+   create_table_statement := trim(trailing ',' from create_table_statement);
+   create_table_statement := create_table_statement || ')';
+   execute create_table_statement;
+ end;
+$$ LANGUAGE plpgsql;
+
+select * from health_recomm.convert_to_wide_format();
