fc8ebb4031788d0fbafa74ff4c3c55e62e92ffba
Bibudh Lahiri <bibudhlahiri@gmail.com>
2014-08-11 16:59:28 -0400

    Added function summarize_diag_codes

--- code/recomm_system/reduce_features.R
+++ code/recomm_system/reduce_features.R
@@ -1,5 +1,18 @@
+library(RPostgreSQL)
+
+trim_comma <- function(x)
+{
+  if (substring(x, nchar(x), nchar(x)) == ',')
+  {
+    return(substring(x, 1, nchar(x)-1))
+  }
+  x
+}
+
 longest_common_subseq <- function(x, y)
 {
+  x <- sapply(x, trim_comma)
+  y <- sapply(y, trim_comma)
   m <- length(x)
   n <- length(y)
   c <- matrix(nrow = m+1, ncol = n+1)
@@ -31,11 +44,11 @@ longest_common_subseq <- function(x, y)
   #print(c)
   #print(b)
   lcs <<- ""
-  print_lcs(b, x, m+1, n+1)
+  print_lcs(b, x, m+1, n+1, " ")
   lcs
 }
 
-print_lcs <- function(b, x, i, j)
+print_lcs <- function(b, x, i, j, seperator)
 {
   #cat(paste("i = ", i, ", j = ", j, ", b[i, j] = ", b[i, j], "\n", sep = ""))
   if ((i == 1) | (j == 1))
@@ -44,16 +57,38 @@ print_lcs <- function(b, x, i, j)
   }
   if (b[i, j] == 'D')
   {
-    print_lcs(b, x, i-1, j-1)
+    print_lcs(b, x, i-1, j-1, seperator)
     #print(x[i-1])
-    lcs <<- paste(lcs, x[i-1], "", sep = "")
+    lcs <<- paste(lcs, x[i-1], sep = seperator)
   }
   else if (b[i, j] == 'N')
   {
-    print_lcs(b, x, i-1, j)
+    print_lcs(b, x, i-1, j, seperator)
   }
   else
   {
-    print_lcs(b, x, i, j-1)
+    print_lcs(b, x, i, j-1, seperator)
   }
 }
+
+summarize_diag_codes <- function()
+ {
+   con <- dbConnect(PostgreSQL(), user="postgres", password = "impetus123",  
+                   host = "localhost", port="5432", dbname = "DE-SynPUF")
+   statement <- paste("select a.diagnosis_code as diagnosis_code_1, a.long_desc as long_desc_1, 
+                       b.diagnosis_code as diagnosis_code_2, b.long_desc long_desc_2
+                       from diagnosis_codes a, diagnosis_codes b
+                       where substring(a.diagnosis_code from 1 for 3) = substring(b.diagnosis_code from 1 for 3)
+                       and a.diagnosis_code <> b.diagnosis_code
+                       --and a.long_desc like 'Malignant neoplasm%'
+                       --and b.long_desc like 'Malignant neoplasm%'
+                       ", sep = "")
+  res <- dbSendQuery(con, statement)
+  df <- fetch(res, n = -1)
+  df <- df[1:500, ]
+  df$lcs <- apply(df, 1, function(row)longest_common_subseq(unlist(strsplit(row["long_desc_1"], " ")), unlist(strsplit(row["long_desc_2"], " "))))
+  print(df$lcs)
+  dbDisconnect(con)
+}
+
+
