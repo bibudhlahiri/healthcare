fe87dc5d5becd83a56c0d14ae5d69d240929db82
Bibudh Lahiri <bibudhlahiri@gmail.com>
2015-04-01 16:35:09 -0400

    Added code in evalFunc for interaction terms

--- code/recomm_system/classify_tcga.R
+++ code/recomm_system/classify_tcga.R
@@ -224,12 +224,17 @@ check_stat_signif <- function()
 {
   data_package <-  prepare_data()
   dense_matrix <- data_package[["dense_matrix"]]
+  drug_vars <- c("Avastin", "BCNU", "CCNU", "CPT.11", "Dexamethasone", "Gliadel.Wafer",  
+                           "Other_drug", "Tarceva", "Temozolomide", "VP.16")
+  radiation_vars <- c("EXTERNAL.BEAM", "Other_radiation")
 
   for (column1 in colnames(dense_matrix))
   {
     for (column2 in colnames(dense_matrix))
     {
-      if (is.factor(dense_matrix[, column1]) & is.factor(dense_matrix[, column2]) & column1 != column2)
+      if (is.factor(dense_matrix[, column1]) 
+          & (is.factor(dense_matrix[, column2]) || column2 %in% c(drug_vars, radiation_vars)) 
+          & column1 != column2)
       {
         M <-  table(dense_matrix[, column1], dense_matrix[, column2])
         Xsq <- chisq.test(M, simulate.p.value = TRUE, B = 9999)
@@ -285,13 +290,18 @@ classify_lr <- function(dense_matrix, response_var)
          str_formula <- paste(str_formula, column, " + ", sep = "")
        }
    }
-   str_formula <- substring(str_formula, 1, nchar(str_formula) - 2)
+   #str_formula <- substring(str_formula, 1, nchar(str_formula) - 2)
 
    #Interaction terms will be necessary to make sure the variables out of demographic and case history variables do not give rise to a constant.
+   #We keep the interaction terms among the demographic-case history variables and the treatment variables only.
    #Once we have the interaction terms (restricted to 2 x 2), we can still use linear program to optimize the objective function.
 
-   #str_formula <- paste(str_formula, " + gender:prior_glioma + histological_type:history_of_neoadjuvant_treatment + 
-   #                                   histological_type:prior_glioma + person_neoplasm_cancer_status:race", sep = "") 
+   str_formula <- paste(str_formula, " histological_type:Temozolomide + histological_type:EXTERNAL.BEAM + 
+                                       history_of_neoadjuvant_treatment:Avastin + history_of_neoadjuvant_treatment:EXTERNAL.BEAM + 
+                                       initial_pathologic_diagnosis_method:Avastin + initial_pathologic_diagnosis_method:CCNU + 
+                                       initial_pathologic_diagnosis_method:CPT.11 + initial_pathologic_diagnosis_method:Other_drug + 
+                                       initial_pathologic_diagnosis_method:Temozolomide + initial_pathologic_diagnosis_method:VP.16 + 
+                                       initial_pathologic_diagnosis_method:EXTERNAL.BEAM + person_neoplasm_cancer_status:Other_drug", sep = "") 
    
    #Problem: With regular logistic regression, only age_at_initial_pathologic_diagnosis and karnofsky_performance_score are statistically significant predictors, 
    #although other predictors (e.g., person_neoplasm_cancer_statusTUMOR FREE) have high values of coefficients. One reason may be scale (?).
@@ -301,7 +311,7 @@ classify_lr <- function(dense_matrix, response_var)
    #If the input training data is same, the Bayesian LR returns the same coefficients and standard errors even if invoked multiple times. 
 
    vital.logr = bayesglm(as.formula(str_formula), family = binomial("logit"), prior.scale = 2.5, prior.df = 1, data = df.train, drop.unused.levels = FALSE, x = TRUE)
-   display(vital.logr)
+   #display(vital.logr)
 
    x.train <- df.train[,!(names(df.train) %in% c(response_var))]
    y.train <- df.train[, response_var]
@@ -590,20 +600,39 @@ convert_bit_string_to_evidence <- function(bit_string, treatment_options, n_opti
 
 evalFunc <- function(bit_string)
 {
-  inputs_for_demog_ch <- c("intercept_coeff" = 1, "age_at_initial_pathologic_diagnosis" = 59, "ethnicityHISPANIC OR LATINO" = 0, "genderMALE" = 1, 
+  inputs_for_demog_ch <- c("intercept_coeff" = 1, "age_at_initial_pathologic_diagnosis" = 59, "ethnicityHISPANIC OR LATINO" = 0, "genderMALE" = 0, 
                            "histological_typeTreated primary GBM" = 1, "histological_typeGlioblastoma Multiforme (GBM)" = 0,
                            "history_of_neoadjuvant_treatmentNo" = 1, "initial_pathologic_diagnosis_methodExcisional Biopsy" = 0,
-                           "initial_pathologic_diagnosis_methodIncisional Biopsy" = 1, "initial_pathologic_diagnosis_methodOther method, specify:" = 0,
-                           "initial_pathologic_diagnosis_methodFine needle aspiration biopsy" = 0, "karnofsky_performance_score" = 20,
+                           "initial_pathologic_diagnosis_methodIncisional Biopsy" = 1, "initial_pathologic_diagnosis_methodOther method, specify" = 0,
+                           "initial_pathologic_diagnosis_methodFine needle aspiration biopsy" = 0, "karnofsky_performance_score" = 80,
                            "person_neoplasm_cancer_statusTUMOR FREE" = 0, "prior_gliomaYES" = 0, "raceBLACK OR AFRICAN AMERICAN" = 0, 
-                           "raceASIAN" = 1)
+                           "raceASIAN" = 0)
   file_path <- "/Users/blahiri/healthcare/data/tcga/Raw_Data/v3"
   load(paste(file_path, "/vital_logr.rda", sep = ""))
   obj_func <- as.numeric(vital.logr$coefficients[1:16]) %*% as.numeric(inputs_for_demog_ch)
 
   obj_func <- obj_func + as.numeric(vital.logr$coefficients[17:28]) %*% bit_string
+
+  #Compute the part of the (quadratic) combination arising out of the interaction terms
+  sum_of_part <- 0
+  interaction_terms <- names(vital.logr$coefficients[29:63])
+
+  drug_vars <- c("Avastin", "BCNU", "CCNU", "CPT.11", "Dexamethasone", "Gliadel.Wafer",  
+                           "Other_drug", "Tarceva", "Temozolomide", "VP.16")
+  radiation_vars <- c("EXTERNAL.BEAM", "Other_radiation")
+  treatment_options <- sort(append(drug_vars, radiation_vars))
+
+  for (interaction_term in interaction_terms)
+  {
+    variables <- unlist(strsplit(interaction_term, ":"))
+    demog_ch_var <- variables[1]
+    treatment_option <- variables[2]
+    sum_of_part <- sum_of_part + inputs_for_demog_ch[[demog_ch_var]]* bit_string[which(treatment_options == treatment_option)]  
+  }
+  obj_func <- obj_func + sum_of_part
+
   n_bits <- length(bit_string)
-  if ((sum(bit_string) > 3) || (sum(bit_string[(n_bits-1): n_bits]) == 2))
+  if ((sum(bit_string) > 3) || (bit_string[which(treatment_options == "EXTERNAL.BEAM")] + bit_string[which(treatment_options == "Other_radiation")] == 2))
   {
     return(0)
   }
@@ -619,9 +648,12 @@ genetic_algorithm_for_optimal <- function(drug_vars, radiation_vars)
   treatment_options <- sort(append(drug_vars, radiation_vars))
   n_options <- length(treatment_options)
 
-  GAmodel <- rbga.bin(size = n_options, popSize = 200, iters = 1, 
-                      mutationChance = 1/(n_options + 1), elitism = T, evalFunc = evalFunc, verbose = TRUE)
+  GAmodel <- rbga.bin(size = n_options, popSize = 200, iters = 7, 
+                      #mutationChance = 1/(n_options + 1), 
+                      mutationChance = 0.01,
+                      elitism = T, evalFunc = evalFunc, verbose = FALSE)
   df <- process_output_of_genetic(GAmodel, treatment_options)
+  model_data <- list("GAmodel" = GAmodel, "df" = df)
 }
 
 #Rank the chromosomes of the final population by evaluation. Since the GA 
@@ -630,9 +662,10 @@ genetic_algorithm_for_optimal <- function(drug_vars, radiation_vars)
 
 process_output_of_genetic <- function(GAmodel, treatment_options)
 {
+  library(boot)
   df <- as.data.frame(GAmodel$population)
   df$evaluation <- GAmodel$evaluations
-  df$surv_prob <- invlogit(-df$evaluation)
+  df$surv_prob <- inv.logit(-df$evaluation)
   df <- df[order(-df[, "surv_prob"]),]
   df <- df[!duplicated(df), ]
   df$combination <- apply(df, 1, function(row) convert_bit_string_to_text(row[paste("V", 1:12, sep = "")], treatment_options)) 
@@ -657,7 +690,7 @@ binary_programming_for_optimal <- function(vital.model)
   constraint_for_demog_ch <- c("age_at_initial_pathologic_diagnosis" = 40, "ethnicityHISPANIC OR LATINO" = 0, "genderMALE" = 1, 
                                "histological_typeTreated primary GBM" = 1, "histological_typeGlioblastoma Multiforme (GBM)" = 0,
                                "history_of_neoadjuvant_treatmentNo" = 1, "initial_pathologic_diagnosis_methodExcisional Biopsy" = 1,
-                               "initial_pathologic_diagnosis_methodIncisional Biopsy" = 0, "initial_pathologic_diagnosis_methodOther method, specify:" = 0,
+                               "initial_pathologic_diagnosis_methodIncisional Biopsy" = 0, "initial_pathologic_diagnosis_methodOther method, specify" = 0,
                                "initial_pathologic_diagnosis_methodFine needle aspiration biopsy" = 0, "karnofsky_performance_score" = 30,
                                "person_neoplasm_cancer_statusTUMOR FREE" = 0, "prior_gliomaYES" = 1, "raceBLACK OR AFRICAN AMERICAN" = 0, 
                                "raceASIAN" = 0)
@@ -790,8 +823,7 @@ run_show <- function()
   #print(sort(vital.logr$coefficients, decreasing = TRUE)) #Gives the predictors in decreasing order of coefficients, first few are Temozolomide, initial_pathologic_diagnosis_method, Irinotecan and Procarbazine
 
   #The optimum is the chromosome for which the evaluation value is minimal
-  GAmodel <- genetic_algorithm_for_optimal(drug_vars, radiation_vars)
-  
+  model_data <- genetic_algorithm_for_optimal(drug_vars, radiation_vars)
 }
 
 
