529837d623e00e7909d5597ee559fdeb410e1f11
Bibudh Lahiri <bibudhlahiri@gmail.com>
2016-01-15 16:54:43 -0500

    Created sample with equal number of benign and anomalous cases

--- code/cloudera_challenge/analyze_anomaly_with_spark.R
+++ code/cloudera_challenge/analyze_anomaly_with_spark.R
@@ -1,24 +1,35 @@
 
 train_validate_test_rpart <- function()
  {
-   #sc <- sparkR.init(master = "local", sparkPackages = "com.databricks:spark-csv_2.11:1.0.3")
-   #sqlContext <- sparkRSQL.init(sc)
-
    set.seed(1)
-   customSchema <- structType(
-    structField("patient_id", "integer"),
-    structField("age_group", "string"),
-    structField("gender", "string"),
-    structField("income_range", "string"),
-    structField("is_anomalous", "integer"),
-    structField("12", "string"),
-    structField("13", "string"))
-   pat_proc <- read.df(sqlContext, path = "/Users/blahiri/healthcare/data/cloudera_challenge/pat_proc_tiny_few_columns.csv", 
+   pat_proc <- read.df(sqlContext, path = "/Users/blahiri/healthcare/data/cloudera_challenge/pat_proc_larger.csv", 
                        source = "com.databricks.spark.csv", 
-                       #inferSchema = "true", 
-                       header = "true", delimiter = ",", charset = "UTF-8", schema = customSchema)
-   print(head(select(pat_proc, pat_proc$income_range)))
-   print(colnames(pat_proc)) #returns correct results
+                       inferSchema = "true", 
+                       header = "true", delimiter = ",", charset = "UTF-8") #DAG visualization shows this simply calls textFile() and mapPartitions() under the hood
+   
+   anom <- filter(pat_proc, (pat_proc$is_anomalous == '1')) #Filter does not call any new Spark job!
+   benign <- filter(pat_proc, (pat_proc$is_anomalous == '0'))
+   cat(paste("nrow(anom) = ", nrow(anom), ", nrow(benign) = ", nrow(benign), "\n", sep = ""))
+   n_benign <- nrow(benign) 
+   
+   #DAG visualization shows nrow() calls textFile(), mapPartitions(), flatMap(), mapPartitions(), TungstenAggregate(), TungstenExchange(), TungstenExchange(), TungstenAggregate() and map() in sequence.
+   
+   sample_from_benign <- sample(benign, withReplacement = FALSE, fraction = 50000/n_benign)
+   pat_proc <- rbind(anom, sample_from_benign)
+   n_pat_proc <- nrow(pat_proc)
+   
+   features <- setdiff(names(pat_proc), c("patient_id", "is_anomalous"))
+   x <- select(pat_proc, features)
+   y <- select(pat_proc, "is_anomalous")
+   
+   pat_proc_test <- sample(pat_proc, FALSE, 0.5)
+   test_ids <- (collect(select(pat_proc_test, "patient_id")))$patient_id
+   cat(paste("length(test_ids) = ", length(test_ids), "\n", sep = ""))
+   pat_proc$IS_TEST <- (pat_proc$patient_id %in% test_ids)
+   print(pat_proc$IS_TEST)
+   cat(paste("length(pat_proc$IS_TEST) = ", length(pat_proc$IS_TEST), "\n", sep = ""))
+   pat_proc_train <- subset(pat_proc, pat_proc$IS_TEST == FALSE)
+   cat(paste("Size of training data = ", length(pat_proc_train), ", size of test data = ", (nrow(pat_proc) - length(pat_proc_train)), "\n", sep = ""))
  }
 
 
