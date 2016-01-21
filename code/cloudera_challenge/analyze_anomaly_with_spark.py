150d68d46b79545e1f8241e177661af5745d7eb3
Bibudh Lahiri <bibudhlahiri@gmail.com>
2016-01-21 15:08:39 -0500

    Done upto splitting into training and test data

--- code/cloudera_challenge/analyze_anomaly_with_spark.py
+++ code/cloudera_challenge/analyze_anomaly_with_spark.py
@@ -1,20 +1,38 @@
-from pyspark import SparkContext
-
+#from pyspark import SparkConf, SparkContext
+#conf = (SparkConf().setMaster("local[*]").setAppName("AnalyzeAnomaly").set("spark.executor.memory", "5g"))
+#sc = SparkContext(conf = conf)
+from __future__ import division
 
 sc.addPyFile('python/pyspark/pyspark_csv.py')
 import pyspark_csv as pycsv
 
 import sys, traceback
 
-
 def train_validate_test_rpart():
   try:
-    
-    plaintext_rdd = sc.textFile("file:///Users/blahiri/healthcare/data/cloudera_challenge/pat_proc_larger.csv")
+    #sc.setMaster("local[*]").setAppName("AnalyzeAnomaly").set("spark.executor.memory", "5g")
+    plaintext_rdd = sc.textFile("file:///Users/blahiri/healthcare/data/cloudera_challenge/pat_proc_larger.csv") #69.2 MB
     pat_proc = pycsv.csvToDataFrame(sqlContext, plaintext_rdd, sep = ",")
-    print type(pat_proc) #<class 'pyspark.sql.dataframe.DataFrame'>
-    print pat_proc.count() #246948, excludes header
-    print pat_proc.take(1) #prints first data row
+    #print type(pat_proc) #<class 'pyspark.sql.dataframe.DataFrame'>
+    #print pat_proc.count() #246948, excludes header
+    #print pat_proc.take(1) #prints first data row
+    
+    anom = pat_proc.filter(pat_proc.is_anomalous == '1')
+    benign = pat_proc.filter(pat_proc.is_anomalous == '0')
+    n_benign = benign.count()
+    #print("anom.count() = " + str(anom.count()) + ", benign.count() = " + str(benign.count())) #anom.count() = 49542, benign.count() = 197406
+    
+    sample_from_benign = benign.sample(False, 50000/n_benign)
+    print("sample_from_benign.count() = " + str(sample_from_benign.count())) #49,998
+    pat_proc = anom.unionAll(sample_from_benign)
+    print("pat_proc.count() = " + str(pat_proc.count())) #99,227
+    
+    all_columns = pat_proc.columns
+    features = [x for x in all_columns if (x not in ["patient_id", "is_anomalous"])]
+    pat_proc = pat_proc.rdd
+    (train, test) = pat_proc.randomSplit([0.5, 0.5])
+    #model = DecisionTree.trainClassifier(train, numClasses = 2, categoricalFeaturesInfo={}, impurity = 'gini', maxDepth = 5, maxBins = 32)
+    
   except Exception:
     print("Exception in user code:")
     traceback.print_exc(file = sys.stdout)
