4f4fe02605cba9fa58bb0a2b6fd838893c99d512
Bibudh Lahiri <bibudhlahiri@gmail.com>
2016-04-15 11:06:53 -0400

    Added method map_categorical_to_numeric, brought in com.databricks.spark.csv for reading CSV

--- code/cloudera_challenge/analyze_anomaly_full_data.py
+++ code/cloudera_challenge/analyze_anomaly_full_data.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
+from pyspark.sql import Row
 import sys, traceback 
 
 home_folder = "/Users/blahiri"
@@ -12,16 +13,7 @@ if __name__ == "__main__":
  import pyspark_csv as pycsv
  sqlContext = SQLContext(sc)
  
- def prepare_data():
-  try:
-    patients_rdd = sc.textFile("file://" + home_folder + "/healthcare/data/cloudera_challenge/patients.csv") #~100 M patients, 2.8 GB, this statement runs in 1 sec
-    #pat_proc = pycsv.csvToDataFrame(sqlContext, plaintext_rdd, sep = ",") #This statement took several hours, launched 89 tasks and then the worker went dead
-    
-    procedures_rdd = sc.textFile("file://" + home_folder + "/healthcare/data/cloudera_challenge/PCDR2011/*.csv") #Even with this, and csvToDataFrame() commented, script took 1 sec to run, 
-    #so sc.textFile() is actually lazy
-    #print("patients_rdd.count() = " + str(patients_rdd.count()) + ", procedures_rdd.count() = " + str(procedures_rdd.count())) #This brought back the 89 tasks
-    
-    #Prepare data in LIBSVM format
+ def get_values_of_categorical_vars(patients_rdd):
     age_groups = patients_rdd.map(lambda x:x.split(",")).map(lambda x: x[1]).distinct() #3.8 min
     print("age_groups.count() = " + str(age_groups.count())) #2 s
     str_age_groups = age_groups.reduce(lambda x, y: x + "; " + y) #0.6 s
@@ -34,12 +26,54 @@ if __name__ == "__main__":
     str_income_groups = income_groups.reduce(lambda x, y: x + "; " + y)
     print("str_age_groups = " + str_age_groups + ", str_genders = " + str_genders + ", str_income_groups = " + str_income_groups)
     #str_age_groups =  85+;  65-74;  75-84;  <65, str_genders =  M;  F, str_income_groups =  <16000;  48000+;  32000-47999;  24000-31999;  16000-23999
+    return   
+    
+ def map_categorical_to_numeric(row, age_groups, genders, income_groups):
+   #Map each categorical feature to a numeric value. 
+   age_group = ((row.asDict())["age_group"]).encode('ascii','ignore')
+   print("age_group before conv = " + age_group)
+   age_group = age_groups.index(age_group)
+   print("age_group after conv = " + str(age_group))
+   
+   gender = ((row.asDict())["gender"]).encode('ascii','ignore')
+   print("gender before conv = " + gender)
+   gender = genders.index(gender)
+   print("gender after conv = " + str(gender))
+   
+   income_group = ((row.asDict())["income_group"]).encode('ascii','ignore')
+   print("income_group before conv = " + income_group)
+   income_group = income_groups.index(income_group)
+   print("income_group after conv = " + str(income_group))
+   
+   return
+    
+ def prepare_data():
+  try:
+    patients_df = sqlContext.read.format("com.databricks.spark.csv").option("header", "false").load("file://" + home_folder + "/healthcare/data/cloudera_challenge/patients.csv")
+    oldColumns = patients_df.schema.names
+    newColumns = ["patient_id", "age_group", "gender", "income_group"]
+    patients_df = reduce(lambda data, idx: data.withColumnRenamed(oldColumns[idx], newColumns[idx]), xrange(len(oldColumns)), patients_df)
+    
+    #age_groups = patients_df.select(patients_df["age_group"].cast("string")).distinct().collect()
+    
+    procedures_rdd = sc.textFile("file://" + home_folder + "/healthcare/data/cloudera_challenge/PCDR2011/*.csv") #Even with this, and csvToDataFrame() commented, script took 1 sec to run, 
+    #so sc.textFile() is actually lazy
+    
+    reviews_rdd = sc.textFile("file://" + home_folder + "/healthcare/data/cloudera_challenge/REVIEW.TXT")
+    
+    #Prepare data in LIBSVM format
+    
     #Idea: Use lists created out of these hard-coded values for now and look up the index of a value in the list
+    
+    age_groups = [" <65", " 65-74", " 75-84", " 85+"]
+    genders = [" M", " F"]
+    income_groups = [" <16000", " 16000-23999", " 24000-31999", " 32000-47999", " 48000+"]  
+    patients_processed = patients_df.map(lambda x: map_categorical_to_numeric(x, age_groups, genders, income_groups)).collect()
     
   except Exception:
     print("Exception in user code:")
     traceback.print_exc(file = sys.stdout)
-   
+  return 
    
 
 sparse_data = prepare_data()
