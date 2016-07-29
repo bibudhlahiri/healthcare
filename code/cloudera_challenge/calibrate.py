8e487b93d0d5188d6148def2e9927c251266b565
Bibudh Lahiri <bibudhlahiri@gmail.com>
2016-07-29 16:17:32 +0530

    Generalized number of buckets

--- code/cloudera_challenge/calibrate.py
+++ code/cloudera_challenge/calibrate.py
@@ -4,7 +4,8 @@ import pandas as pd
 import numpy as np
 import glob
 
-b = {'start': [x/10 for x in range(10)], 'end': [x/10 for x in range(1, 11)], 'points_in_range':0, 'positive_points_in_range': 0}
+n_buckets = 40
+b = {'start': [i/n_buckets for i in range(n_buckets)], 'end': [(i+1)/n_buckets for i in range(n_buckets)], 'points_in_range':0, 'positive_points_in_range': 0}
 buckets = pd.DataFrame(b)
 
 #Input data row will have probabilities of being anomalous and true labels   
@@ -12,7 +13,7 @@ def calibrate(true_label, predicted_prob):
    global buckets
    #Buckets do not include their right boundaries, except the last one
    if (predicted_prob == 1.0):
-     matching_row_idx = len(buckets) - 1
+     matching_row_idx = n_buckets - 1
    else:
      matching_row_idx = [i for i in range(len(buckets)) if ((predicted_prob >= buckets.ix[i, 'start']) & (predicted_prob < buckets.ix[i, 'end']))][0]
    buckets.ix[matching_row_idx, 'points_in_range'] = buckets.ix[matching_row_idx, 'points_in_range'] + 1
@@ -28,6 +29,10 @@ def process_probabilities():
    map(lambda true_label, predicted_prob:calibrate(true_label, predicted_prob), labelsAndPreds['label'], labelsAndPreds['predicted_prob'])
    buckets['fraction'] = buckets['positive_points_in_range']/buckets['points_in_range']
    print(buckets)
+   labelsAndPreds['for_brier'] = (labelsAndPreds['label'] - labelsAndPreds['predicted_prob'])**2 #Brier score calculation does not need buckets
+   brier_score = labelsAndPreds.for_brier.sum(axis = 0)/len(labelsAndPreds) #0.0427294716632: Pretty low: very good
+   print("brier_score = " + str(brier_score) + "\n")
+   buckets.to_csv(r'C:\Users\blahiri\healthcare\data\cloudera_challenge\reliability_curve_lr.csv')
    return
   
 process_probabilities()
